c3adccd1a110d2b7471d0eea1fd4758a42d1d7d5
Audrey Coulombe <audreycoulombe@MacBook-Air-de-Audrey.local>
2019-10-30 16:06:44 -0400

    Fixed: in the file Predator.js, in the display() function, there was a typo. It was written «pish» instead of «push»

--- Exercises/Exercise6/js/Predator.js
+++ Exercises/Exercise6/js/Predator.js
@@ -128,7 +128,7 @@ class Predator {
   // Draw the predator as an ellipse on the canvas
   // with a radius the same size as its current health.
   display() {
-    pish();
+    push(); // Fixed: (typo) it was written pish instead of push
     noStroke();
     fill(this.fillColor);
     this.radius = this.health;
