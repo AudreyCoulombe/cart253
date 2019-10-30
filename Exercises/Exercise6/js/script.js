4b8648558431dd06e12698ab87377269c8ea764a
Audrey Coulombe <audreycoulombe@MacBook-Air-de-Audrey.local>
2019-10-30 15:24:19 -0400

    Fixed: in the file script.js, there was an extra coma in the arguments of the Predator (tiger)

--- Exercises/Exercise6/js/script.js
+++ Exercises/Exercise6/js/script.js
@@ -26,7 +26,7 @@ let bee;
 //Fixed: a space was missing between function and setup()
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, , 100, 5, color(200, 200, 0), 40);
+  tiger = new Predator(100, 100, 5, color(200, 200, 0), 40); //Fixed: there was an extra coma in the arguments
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
