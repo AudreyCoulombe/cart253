2b2d031b00c19d26c7afecda7fdf8774da1d2b6d
Audrey Coulombe <audreycoulombe@MacBook-Air-de-Audrey.local>
2019-10-30 17:30:47 -0400

    Fixed: I couldn't move the tiger up. In the file Predator.js, in the constructor's properties, there was a typo. It was written «appKey» instead of «upKey»

--- Exercises/Exercise6/js/Predator.js
+++ Exercises/Exercise6/js/Predator.js
@@ -33,7 +33,7 @@ class Predator {
     this.fillColor = fillColor;
     this.radios = this.health; // Radius is defined in terms of health
     // Input properties
-    this.appKey = UP_ARROW;
+    this.upKey = UP_ARROW; // Fixed: (typo) it was written appKey instead of upKey
     this.downKey = DOWN_ARROW;
     this.leftKey = LEFT_ARROW;
     this.rightKey = RIGHT_ARROW;
