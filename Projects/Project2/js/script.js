3d794c5b07edbf242929d42a2218684ff7078398
Audrey Coulombe <audreycoulombe@air-de-audrey.wireless.concordia.ca>
2019-10-31 16:29:28 -0400

    P2: changed images so the point of view is in the water instead of over the water. Also added the Jaws theme song .

--- Projects/Project2/js/script.js
+++ Projects/Project2/js/script.js
@@ -20,28 +20,37 @@ let seahorseImage;
 let backgroundWaterImage;
 let foregroundNenupharImage;
 
+// Sound
+let jawsThemeSFX;
+
 // preload()
 //
 // Preload images and sounds
 function preload() {
+  // images
   sharkImage = loadImage("assets/images/shark.png");
   yellowFishImage = loadImage("assets/images/yellowFish.png");
   greyFishImage = loadImage("assets/images/greyFish.png");
   seahorseImage = loadImage("assets/images/seahorse.png");
   backgroundWaterImage = loadImage("assets/images/background.jpg");
   foregroundNenupharImage = loadImage("assets/images/foreground.png");
+  // sounds
+  jawsThemeSFX = loadSound("assets/sounds/JawsTheme.mp3");
 }
 
 // setup()
 //
 // Sets up a canvas
 // Creates objects for the predator and three prey
+// plays the sound in loop
 function setup() {
   createCanvas(windowWidth, windowHeight);
   shark = new Predator(100, 100, 5, color(200, 200, 0), 40);
   yellowFish = new Prey(100, 100, 10, color(255, 100, 10), 50);
   greyFish = new Prey(100, 100, 8, color(255, 255, 255), 60);
   seahorse = new Prey(100, 100, 20, color(255, 255, 0), 10);
+  // play the sound in loop
+  jawsThemeSFX.loop();
 }
 
 // draw()
